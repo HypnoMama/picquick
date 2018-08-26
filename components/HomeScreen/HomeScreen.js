6d49f41372948cbe90fdcc96b82c3c78e9eb5c1c
HypnoMama <sarahhermsen@gmail.com>
2018-08-26 00:03:11 +0000

    added full screen styling to camera

--- components/HomeScreen/HomeScreen.js
+++ components/HomeScreen/HomeScreen.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import MyCamera from './Camera';
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View>
-        <MyCamera />
+      <View style={styles.container}>
+       
+
+        <MyCamera  />
+        
       </View>
   )}
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    width: '100%',
+    height: '100%', 
+  }
+
+})
