8a2bfcc71832d9b2752281a07e9d3f45134dd65b
HypnoMama <sarahhermsen@gmail.com>
2018-08-26 05:28:07 +0000

    removed random semi-colon and changed testing text

--- components/HomeScreen/Modal/Modal.js
+++ components/HomeScreen/Modal/Modal.js
@@ -8,27 +8,18 @@ export default class Modal extends React.Component {
   listItems = this.props.predictions.outputs[0].data.concepts
 
 
-  renderItems(listItems) {;
+  renderItems(listItems) {
     return listItems.map(listItem => <Ingredient item={listItem} key={listItem.value} />);    
   }
 
   render() {
-
-    // {console.log(listItems)}
-
-    // const renderItems = (listItems) = {
-    //   listItems.map((listItem) => {
-    //     return <Ingredient name={listItem.name} value={listItem.value} />
-    //   });
-    // }
-
    
 
     return (
       <View >
       
         <Text style={{paddingTop: 50}}>Hello I am a Modal!!!</Text>
-        <Text>yahoo</Text>
+        <Text>Booyah</Text>
         {this.renderItems(this.listItems)}
         {/* <Text>{console.log({this.props.predictions})</Text> */}
       </View>
