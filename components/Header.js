62742b5b2351386d8de6b43a8a67fd003918d6e8
HypnoMama <sarahhermsen@gmail.com>
2018-08-26 03:47:58 +0000

    added a simple header

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = () => {
+  return (
+    <View>
+      <Text style={{marginTop: 50}}>PicQuick</Text>
+    </View>
+  )
+}
+
+export default Header
