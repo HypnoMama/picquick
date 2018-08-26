d989a3dd083cf9831c600cbcf42141bb07add167
HypnoMama <sarahhermsen@gmail.com>
2018-08-26 03:50:13 +0000

    initialized an Ingredient component

---
+++ components/HomeScreen/Modal/Ingredient.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Ingredient = (props) => {
+  return (
+
+    <View>
+      <Text>{this.props.name}</Text>
+      {/* <Text>{this.props.value}</Text> */}
+    </View>
+  )
+}
+
+export default Ingredient;
