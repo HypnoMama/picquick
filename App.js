94b1d31c35ff1fc3cce278bbd1a135334f7a21b5
HypnoMama <sarahhermsen@gmail.com>
2018-08-26 00:50:39 +0000

    intial commit of App

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Camera from './components/HomeScreen/Camera';
+// import Camera from './components/HomeScreen/Camera';
 import HomeScreen from './components/HomeScreen/HomeScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Camera />
+        <HomeScreen />
       </View>
     );
   }
