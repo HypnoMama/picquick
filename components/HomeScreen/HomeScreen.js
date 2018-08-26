f2b348136904c699cc8c3f7f2f215903b901db54
HypnoMama <sarahhermsen@gmail.com>
2018-08-26 03:48:52 +0000

    rendering components based on state

--- components/HomeScreen/HomeScreen.js
+++ components/HomeScreen/HomeScreen.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import Header from '../Header';
 import MyCamera from './Camera';
+import Modal from './Modal/Modal';
 
 export default class HomeScreen extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      predictions: [],
+      cameraClicked: false
     }
     this.getPredictions = this.getPredictions.bind(this);
   }
 
 
   getPredictions(clarifaiData) {
-    this.setState({predictions: clarifaiData})
-    
+    this.setState({predictions: clarifaiData})   
   }
 
 
   render() {
+
+    let theComponent;
+      // {this.state.predictions} ? 
+      // theComponent = <MyCamera getPredictions={this.getPredictions} />
+      // : theComponent = <Modal />
+
+      if (!this.state.predictions) {
+        alert("No state of Predictions")
+        theComponent = <MyCamera getPredictions={this.getPredictions} />
+      } else {  
+        alert("State!") 
+        // console.log(this.state.predictions)
+        theComponent = <Modal predictions={this.state.predictions}/>
+      }
+
+
+
     return (
       <View style={styles.container}>
-      
-        <MyCamera getPredictions={this.getPredictions} />
-        
+      <Header />
+
+        {theComponent}
+    
       </View>
   )}
 }
