363434da066baf81a5ab215c83a29dd25e0b33d1
HypnoMama <sarahhermsen@gmail.com>
2018-08-27 01:27:37 +0000

    cleaned up dead code

--- components/HomeScreen/Modal/Modal.js
+++ components/HomeScreen/Modal/Modal.js
@@ -3,7 +3,6 @@ import { View, Text, FlatList } from 'react-native';
 import Ingredient from './Ingredient';
 
 
-
 export default class Modal extends React.Component {
 
   constructor(props){
@@ -22,31 +21,22 @@ export default class Modal extends React.Component {
   } 
 
   listItems = this.props.predictions.outputs[0].data.concepts;
+  
   _keyExtractor = (item, index) => item.id;
-
- 
-
-
-  // renderItems(listItems) {
-  //   return listItems.map(listItem => <Ingredient item={listItem} key={listItem.value} />);    
-  // }
 
   render() {
    
 
     return (
       <View >
-      
-        <Text style={{paddingTop: 50}}>Hello I am a Modal!!!</Text>
-        <Text>Booyah</Text>
+ 
         <FlatList 
           data={this.listItems}
           keyExtractor={this._keyExtractor}
           extraData={this.state}
-          renderItem={({item, index}) => (<Ingredient item={item} allItems={this.listItems} index={index} parentFlatList={this} />)}
-        
+          renderItem={({item, index}) => (<Ingredient item={item} allItems={this.listItems} index={index} parentFlatList={this} />)}        
         />
-        {/* {this.renderItems(this.listItems)} */}
+
       </View>
     )
   }
