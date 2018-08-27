70ab9fab807aec59729f811516b0ff0c758c298b
samueljohnwilson <samjwilson@gmail.com>
2018-08-27 16:13:19 +0000

    Update boilerplate to exclude unnecessary code

--- components/RecipeScreen/RecipeCard.js
+++ components/RecipeScreen/RecipeCard.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import Header from '../Header';
+
+const users = [
+ {
+    name: 'brynn',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+ },
+]
 
 export default class RecipeCard extends React.Component {
 
@@ -9,52 +16,16 @@ export default class RecipeCard extends React.Component {
 
     return(
 
-    <View>
-      // implemented without image with header
-      <Card title="CARD WITH DIVIDER">
-        {
-          users.map((u, i) => {
-            return (
-              <View key={i} style={styles.user}>
-                <Image
-                  style={styles.image}
-                  resizeMode="cover"
-                  source={{ uri: u.avatar }}
-                />
-                <Text style={styles.name}>{u.name}</Text>
-              </View>
-            );
-          })
-        }
-      </Card>
-
-      // implemented without image without header, using ListItem component
-       <Card containerStyle={{padding: 0}} >
-        {
-          users.map((u, i) => {
-            return (
-              <ListItem
-                key={i}
-                roundAvatar
-                title={u.name}
-                avatar={{uri:u.avatar}}
-              />
-            );
-          })
-        }
-      </Card>
-
-
       // implemented with Text and Button as children
+    <View>
       <Card
-        title='HELLO WORLD'
+        title='HELLO WORLD'>
         <Text style={{marginBottom: 10}}>
           The idea with React Native Elements is more about component structure than actual design.
         </Text>
         <Button
           icon={<Icon name='code' color='#ffffff' />}
           backgroundColor='#03A9F4'
-          fontFamily='Lato'
           buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
           title='VIEW NOW' />
       </Card>
