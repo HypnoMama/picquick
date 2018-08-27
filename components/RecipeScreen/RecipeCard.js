88671083398e92a12b24d755ed177bd7dbad8f3f
samueljohnwilson <samjwilson@gmail.com>
2018-08-27 17:46:08 +0000

    Make API call using fake data

--- components/RecipeScreen/RecipeCard.js
+++ components/RecipeScreen/RecipeCard.js
@@ -6,28 +6,27 @@ import ApiKeys from '../../ApiKeys';
 
 export default class RecipeCard extends React.Component {
 
-    handlePress = async () => {
-    let response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=4`
-    )
-    .then( (response) => response.json())
-    .then( (responseJson) => {
-      Alert.alert("Recipes: " + responseJson.hits[0].recipe.label);
+  componentDidMount() {
+    return fetch(`https://api.edamam.com/search?q=chicken&app_id=${ApiKeys.edamamConfig.APP_ID}&app_key=${ApiKeys.edamamConfig.API_KEY}&from=0&to=4`)
+      .then( (response) => response.json())
+      .then( (responseJson) => {
+        
+        this.setState({
+          dataSource: responseJson.hits,
+        });
       })
 
-    .catch((error) => {
-      //Render an error page instead
-      console.error(error);
-  });
-}
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   render() {
 
     return(
 
     <View>
-      <Card
-        title='I am a recipe!'>
+      <Card>
         <Text style={{marginBottom: 10}}>
           These are recipe ingredient!
         </Text>
@@ -42,14 +41,6 @@ export default class RecipeCard extends React.Component {
 
       </Card>
     </View>
-
-          <View style={{paddingTop: 50, paddingLeft: 50 }}>
-        <Text> Edamam Test </Text>
-        <TouchableOpacity onPress={this.handlePress.bind(this)}>
-          <Text style={{paddingTop: 50, paddingLeft: 50, color: '#FF0000'}}> Click me for recipes</Text>
-        </TouchableOpacity>
-      </View> 
-    );
 
     )
 
