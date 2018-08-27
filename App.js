5bf2f7dcdcf16aba740497efe57ca7894fa936ec
HypnoMama <sarahhermsen@gmail.com>
2018-08-27 23:19:10 +0000

    fixed merge conflict

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
 // import Camera from './components/HomeScreen/Camera';
-=======
->>>>>>> feature/server
 import HomeScreen from './components/HomeScreen/HomeScreen';
 
 export default class App extends React.Component {
