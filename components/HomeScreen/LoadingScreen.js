5a41df5fe284157c0a1f584981646f01758abe99
samueljohnwilson <samjwilson@gmail.com>
2018-08-28 21:26:39 +0000

    Initialize load screen component

---
+++ components/HomeScreen/LoadingScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { Expo, Font } from 'expo';
+
+export default class LoadingScreen extends React.Component {
+
+}
