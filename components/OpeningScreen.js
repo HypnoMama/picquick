072c1868beabaf861cebe57e3437404d5dd420ce
samueljohnwilson <samjwilson@gmail.com>
2018-08-29 02:35:08 +0000

    Add new logo and update layout of opening screen

--- components/OpeningScreen.js
+++ components/OpeningScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, ActivityIndicator, Image } from 'react-native';
-import { Expo, Font } from 'expo';
 
 export default class LoadingScreen extends React.Component {
   render() {
     return (
 
       <View style={styles.viewStyle}>
-        <Image source={require('./../assets/logo.png')} />
+        <Image style={styles.imageStyle} source={require('./../assets/logo.png')} />
+        <Image style={styles.imageStyle2} source={require('./../assets/logo-full.png')} />
       </View>
     )
   }
@@ -15,9 +15,18 @@ export default class LoadingScreen extends React.Component {
 
 const styles = StyleSheet.create ({
   viewStyle: {
-    flex: 1, 
-    padding: 20, 
+    flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#A2E5F4',
+  },
+  imageStyle: {
+    height: 300, 
+    width: 300,
+  },
+    imageStyle2: {
+    height: 100, 
+    width: 200,
   }
 });
