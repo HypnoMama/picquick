4cddd96b2cfa98d47ebd194833cb2500190cb19b
HypnoMama <sarahhermsen@gmail.com>
2018-08-29 20:45:56 +0000

    added route navigation to camera

--- components/HomeScreen/HomeScreen.js
+++ components/HomeScreen/HomeScreen.js
@@ -1,11 +1,8 @@
-
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
-import TheHeader from '../Header';
 import MyCamera from './Camera';
-import ModalScreen from './Modal/Modal';
-import RecipeScreen from './../RecipeScreen/RecipeScreen';
 import OpeningScreen from './../OpeningScreen'
+
 
 export default class HomeScreen extends React.Component {
 
@@ -15,50 +12,24 @@ export default class HomeScreen extends React.Component {
       cameraClicked: false,
       goneBack: false,
       isLoading: true,
+      goneBackToModal: false,
     }
-    this.getPredictions = this.getPredictions.bind(this);
-    this.goneBack = this.goneBack.bind(this);
-    this.goneToModal = this.goneToModal.bind(this);
   }
 
-  
-
-  getPredictions(clarifaiData) {
-    this.setState({predictions: clarifaiData})   
-  }
-
-  goneBack() {
-
-    this.setState({goneBack: !this.state.goneBack, predictions: false})
-    
-  }
-
-  goneToModal() {
-
-    this.setState({goneBackToModal: false, goneBack: true})
-  
   componentDidMount() {
     setTimeout( () => {this.setState({isLoading: false})}, 5000);
   }
 
   render() {
-
     
     let theComponent;
 
     this.state.isLoading ?
       theComponent = <OpeningScreen />
-      :
-      (
-        !this.state.predictions && !this.state.goneBack && !this.state.goneBackToModal ? 
-        theComponent = <MyCamera getPredictions={this.getPredictions} goneBackToCamera={this.goneBack} />
-        : theComponent = <ModalScreen predictions={this.state.predictions} goBackToModalScreen={this.goneToModal} goneBackToCamera={this.goneBack} />
-      )
+      :  theComponent = <MyCamera navigation={this.props.navigation}/>
 
     return (
       <View style={styles.container}>
-
-        {/* <TheHeader goneBackToCamera={this.goneBack} /> */}
 
         {theComponent} 
     
