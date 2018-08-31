57a18b55de67f32ef7774f3b58356dbe69622f14
HypnoMama <sarahhermsen@gmail.com>
2018-08-31 22:00:40 +0000

    drawerContent is in stackNav

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Button } from 'react-native';
 import HomeScreen from './components/HomeScreen/HomeScreen';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator, DrawerItems, DrawerActions } from 'react-navigation';
 import MyCamera from './components/HomeScreen/Camera';
 import RecipeScreen from './components/RecipeScreen/RecipeScreen';
-import ModalScreen from './components/HomeScreen/Modal/Modal'
+import ModalScreen from './components/HomeScreen/Modal/Modal';
+import TheHeader from './components/Header';
+import Drawer from './components/config-drawer-navigation';
+import { Icon } from 'react-native-elements';
+import DrawerContent from "./components/Sidebar";
 
 
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Camera: MyCamera,
-    ModalScreen: ModalScreen,
-    RecipeScreen: RecipeScreen,    
-  },
-  {
-    initialRouteName: 'Home'
-  }
-);
 
 
 export default class App extends React.Component {
 
+
+  
   // componentDidMount() {
   //   this.callBackendAPI()
   //     .then(res => this.setState({ data: res.express }))
@@ -38,17 +33,101 @@ export default class App extends React.Component {
   //   return body;
   // };
 
+  // state = {
+  //   fontLoaded: false,
+  // }
+
+  // async componentDidMount() {
+  //   await Expo.Font.loadAsync({
+  //     'header-font': require('./assets/fonts/JosefinSans-Regular.ttf'),
+  //   });
+
+  //   this.setState({ fontLoaded: true });
+  // }
+
   render() {
-    return ( <RootStack />)
-      // <View style={styles.container}>
-      //   <HomeScreen />
-      // </View>
+
+    
+    return ( 
+    
 
      
-
-  
+        <RootStack /> 
+        
+        
+        
+        
+      
+      // this.state.fontLoaded ?
+    
+    )
   }
 }
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Camera: MyCamera,
+    // Drawer: Drawer,
+    // Drawer: DrawerContent,
+    ModalScreen: {
+      screen: ModalScreen
+      // navigationOptions: ({navigation}) => ({
+        
+      //   headerRight: (
+      //     <TouchableOpacity onPress={() => {navigation.navigate('Drawer')}}>
+      //       <Icon name="menu" type='react-native-vector-icons' size={30} />
+      //     </TouchableOpacity>
+      //   )
+  
+      // })
+    },
+    DrawerScreen: {
+      screen: DrawerContent
+    },
+    RecipeScreen: RecipeScreen,  
+      
+  },
+  {
+    initialRouteName: 'Home',
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: <TheHeader />,
+      headerStyle: {
+        backgroundColor: '#1b8295'
+      },
+     
+      
+    })
+  }
+);
+
+
+
+
+
+
+
+// #1b8295
+
+
+// const drawer = createDrawerNavigator({
+//   Home: {
+//     screen: HomeScreen,
+//   },
+//   Picture: {
+//     screen: MyCamera,
+//   },
+//   ModalScreen: {
+//     screen: ModalScreen,
+//   },
+//   contentComponent: (props) => (
+//         <View>
+//           <TheHeader/>
+//           <DrawerItems {...props} />
+//           <Text>Custom Footer</Text>
+//         </View>
+//   )
+// });
 
 const styles = StyleSheet.create({
   container: {
@@ -57,5 +136,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+const headerStyles = StyleSheet.create({
+  textStyle: {
+    paddingTop: 25,
+    fontSize: 40,
+    color: '#A2E4F4',
+    backgroundColor: '#025265',
+    height: 80,
+    textAlign: 'center',
+    // fontFamily: 'header-font',
+  },
+  viewStyle: {  
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  }
 });
 
